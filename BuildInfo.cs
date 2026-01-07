b729a9183680c1b319512526a4793f34fac588a1
Fuar11 <mcaa6556@gmail.com>
2026-01-06 19:19:14 -0500

    Updated to TLD 2.51

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -11,11 +11,11 @@
         /// <summary>Who made the mod</summary>
         public const string Author                          = "Fuar";
         /// <summary>Current version (Using Major.Minor.Build) </summary>
-        public const string Version                         = "1.2.1";
+        public const string Version                         = "1.2.2";
         /// <summary>Name used on GUI's, like ModSettings</summary>
         public const string GUIName                         = "Reduced Loot";
         /// <summary>The minimum Melon Loader version that your mod requires</summary>
-        public const string MelonLoaderVersion              = "0.6.1";
+        //public const string MelonLoaderVersion              = "0.7.2";
         #endregion
 
         #region Optional
